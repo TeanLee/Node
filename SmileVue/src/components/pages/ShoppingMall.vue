13bfcda6f8bd1842dc8eeb96f7ddacccd0da65f7
TeanLee <ltt598625763@163.com>
2019-05-15 09:46:41 +0800

    search-bar

--- SmileVue/src/components/pages/ShoppingMall.vue
+++ SmileVue/src/components/pages/ShoppingMall.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    {{msg}}
+  <!--search bar layout-->
+  <div class="search-bar">
+    <van-row>
+      <van-col span="3" class="location-icon"><van-icon name="search" /></van-col>
+      <van-col span="16"><input type="text" class="search-input"/></van-col>
+      <van-col span="5"><van-button size="mini">查找</van-button></van-col>
+    </van-row>
   </div>
 </template>
 
@@ -13,3 +18,26 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.search-bar{
+  height: 2.2rem;
+  background-color: #e5017d;
+  line-height:2.2rem;
+
+}
+.search-input{
+  width:100%;
+  height: 1.3rem;
+  border-top:0px;
+  border-left:0px;
+  border-right:0px;
+  border-bottom: 1px solid 1px !important ;
+  background-color: #e5017d;
+  color:#fff;
+}
+.location-icon{
+  text-align: center;
+  line-height: 2.8rem;
+}
+</style>
--- SmileVue/src/main.js
+++ SmileVue/src/main.js
@@ -1,13 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import { Button, Row, Col } from 'vant';
+import { Button, Row, Col, Icon } from 'vant';
 import 'vant/lib/index.css';
 import Vue from 'vue';
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
-Vue.use(Button).use(Row).use(Col);
+Vue.use(Button).use(Row).use(Col).use(Icon);
 
 /* eslint-disable no-new */
 new Vue({
