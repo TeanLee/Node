efc8bd1603607fc0fd04fc7deda4ba8c8793ca53
TeanLee <ltt598625763@163.com>
2019-05-15 09:16:02 +0800

    移动端适配及按需引入vant

--- SmileVue/src/main.js
+++ SmileVue/src/main.js
@@ -1,10 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import { Button, Row, Col } from 'vant';
+import 'vant/lib/index.css';
 import Vue from 'vue';
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
+Vue.use(Button).use(Row).use(Col);
 
 /* eslint-disable no-new */
 new Vue({
