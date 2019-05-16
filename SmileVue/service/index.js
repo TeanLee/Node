7e20351fd60e0ce0c8eefb53f4ab3eb5050acf20
TeanLee <ltt598625763@163.com>
2019-05-16 09:57:16 +0800

    backend-用户表操作的路由模块化

--- SmileVue/service/index.js
+++ SmileVue/service/index.js
@@ -1,4 +1,5 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 
 const app = new Koa();
 // 在操作数据库前先引入我们的Mongoose和我们刚写好的initSchemas
@@ -20,10 +21,19 @@ const { connect, initSchemas } = require('./database/init.js');
 })();
 
 
-app.use(async (ctx) => {
-  ctx.body = '<h1>hello Koa2</h1>';
-});
+// app.use(async (ctx) => {
+//   ctx.body = '<h1>hello Koa2</h1>';
+// });
 
 app.listen(3000, () => {
   console.log('[Server] starting at port 3000');
 });
+
+// 路由加载
+
+// 引入 user 模块
+const user = require('./appApi/user.js');
+
+const router = new Router();
+router.use('/user', user.routes());
+app.use(router.routes()).use(router.allowedMethods());
