82777d247d44e0bfe186af9128b2b872a8ada1bf
TeanLee <ltt598625763@163.com>
2019-05-16 09:35:45 +0800

    注册页面前端视图

--- SmileVue/src/main.js
+++ SmileVue/src/main.js
@@ -1,6 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import { Button, Row, Col, Icon, Swipe, SwipeItem, Lazyload, List } from 'vant';
+import { Button, Row, Col, Icon, Swipe, SwipeItem, Lazyload, List, Field, NavBar } from 'vant';
 import 'vant/lib/index.css';
 import Vue from 'vue';
 import App from './App';
@@ -11,7 +11,9 @@ Vue.use(Button).use(Row).use(Col).use(Icon)
   .use(Swipe)
   .use(SwipeItem)
   .use(Lazyload)
-  .use(List);
+  .use(List)
+  .use(Field)
+  .use(NavBar);
 
 /* eslint-disable no-new */
 new Vue({
